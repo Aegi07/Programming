de0f665d9b66adb76c66ea5b76f211cce62ae6e8
yliu <liuyinzhi1725@gmail.com>
2024-01-19 21:49:33 +0800

    add generator basics

---
+++ Python/generator_basics.py
@@ -0,0 +1,21 @@
+import sys
+
+def gen(x):
+    for i in range(x):
+        yield i
+
+def get_size(generator):
+    return f"size is {sys.getsizeof(generator)}"
+
+vals = gen(10)
+print(get_size(vals))
+
+for i in range(5):
+    print(next(vals))
+
+print(get_size(vals))
+
+for x in vals:
+    print(x)
+
+print(get_size(vals))
